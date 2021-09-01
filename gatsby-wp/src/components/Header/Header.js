ad05cde45d0eab889807f9a989da219c3c28086a
Shams Arefin Khan <robeeen@gmail.com>
2021-09-01 16:07:26 +0600

    Menu Items fetch

--- gatsby-wp/src/components/Header/Header.js
+++ gatsby-wp/src/components/Header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Navigation from '../Navigation/Navigtion';
 import Logo from '../../images/logo.svg';
 import { useMenuQuery } from '../../hooks/useMenuQuery';
 import { Wrapper, Content } from './Header.styles';
 
 const Header =() => {
-    const { site, menu} = useMenuQuery()
+    const { site, wpMenu} = useMenuQuery()
     return (
         <Wrapper>
             <Content>
-                <Link to="/"><img src={Logo} alt={site.siteMetadata.title} /></Link>
+                <Link to="/"><img src={Logo} alt={site.siteMetadata.title} />
+                </Link>
+                <Navigation menu={wpMenu.menuItems.nodes} />
+                
             </Content>
         </Wrapper>
     )
