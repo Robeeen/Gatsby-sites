7b80db8f7fe64d3c690c1d041e7c5de462f9d9cc
Shams Arefin Khan <robeeen@gmail.com>
2021-09-04 10:07:17 +0600

    Update OverlayMenu.js

--- gatsby-wp/src/components/OverlayMenu/OverlayMenu.js
+++ gatsby-wp/src/components/OverlayMenu/OverlayMenu.js
@@ -23,7 +23,7 @@ const OverlayMenu = ({menuOpen, callback}) => {
                         )}
                     </ul>
                     <div className="closeButton" onClick={callback} role="button" tabIndex="0" onKeyDown={callback}>
-                        <img src={CloseButton} alt="close-button" />
+                        <img src={CloseButton} alt="close-button" /> 
                     </div>
                 </div>
             </Overlay>
